c5a7b36f8daab7f18e9a996ba2246243e3b4fbc3
Sijan Shrestha <shresthasijan92@gmail.com>
2025-09-19 10:15:11 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -64,6 +64,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.microsoft.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
